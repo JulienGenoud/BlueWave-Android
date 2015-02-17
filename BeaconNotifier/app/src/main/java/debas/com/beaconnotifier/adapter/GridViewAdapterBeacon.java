4ea6cc658d0c83a24b0ce4a79c734522f841089f
debas <etienne.debas@gmail.com>
2015-02-17 22:42:19 +0100

    add gridview to history fragment

---
+++ BeaconNotifier/app/src/main/java/debas/com/beaconnotifier/adapter/GridViewAdapterBeacon.java
@@ -0,0 +1,55 @@
+package debas.com.beaconnotifier.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import debas.com.beaconnotifier.R;
+import debas.com.beaconnotifier.model.BeaconItemView;
+
+/**
+ * Created by debas on 17/02/15.
+ */
+public class GridViewAdapterBeacon extends BaseAdapter {
+
+    private final Context mContext;
+    private List<BeaconItemView> mBeaconItemViewList;
+
+    public GridViewAdapterBeacon(Context context) {
+        mContext = context;
+
+        mBeaconItemViewList = new ArrayList<>();
+        mBeaconItemViewList.add(new BeaconItemView());
+        mBeaconItemViewList.add(new BeaconItemView());
+        mBeaconItemViewList.add(new BeaconItemView());
+        mBeaconItemViewList.add(new BeaconItemView());
+    }
+
+    @Override
+    public int getCount() {
+        return mBeaconItemViewList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mBeaconItemViewList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.beacon_item_view, parent, false);
+        }
+        return convertView;
+    }
+}

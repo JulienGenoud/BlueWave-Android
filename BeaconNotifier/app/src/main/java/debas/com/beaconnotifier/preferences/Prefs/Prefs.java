2ae871e84823c6452453e761fae21e7962571124
Julien Genoud <juliengenoud@gmail.com>
2015-02-23 18:35:48 +0100

    add preferences design

---
+++ BeaconNotifier/app/src/main/java/debas/com/beaconnotifier/preferences/Prefs/Prefs.java
@@ -0,0 +1,10 @@
+package debas.com.beaconnotifier.preferences.Prefs;
+
+/**
+ * ** Created by julien on 23/02/15.
+ */
+public class Prefs {
+    public static final String[] PREFS = {
+            "Musées", "Ville", "Magasins", "Historique", "Aéroports", "Entreprises", "Non classés"
+    };
+}

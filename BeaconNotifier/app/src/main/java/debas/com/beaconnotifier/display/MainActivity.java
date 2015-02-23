36d4284b667f2af0748f76b1ec548af5e8a1b504
Julien Genoud <juliengenoud@gmail.com>
2015-02-23 17:29:09 +0100

    add material style

--- BeaconNotifier/app/src/main/java/debas/com/beaconnotifier/display/MainActivity.java
+++ BeaconNotifier/app/src/main/java/debas/com/beaconnotifier/display/MainActivity.java
@@ -93,7 +93,6 @@ public class MainActivity extends BaseActivity implements BeaconConsumer, Observ
         setSupportActionBar((Toolbar) mToolbarView);
 
         ((Toolbar) mToolbarView).inflateMenu(R.menu.main);
-
         ViewCompat.setElevation(findViewById(R.id.header), getResources().getDimension(R.dimen.toolbar_elevation));
         mPagerAdapter = new NavigationAdapter(getSupportFragmentManager());
         mPager = (ViewPager) findViewById(R.id.pager);
